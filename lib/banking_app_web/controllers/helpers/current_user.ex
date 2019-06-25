83e28f31cdb896a43c31d569c58c65fe0b4fec7d
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-06-25 02:28:44 -0300

    Add BankingAppWeb.CurrentUser helper

---
+++ lib/banking_app_web/controllers/helpers/current_user.ex
@@ -0,0 +1,9 @@
+defmodule BankingAppWeb.CurrentUser do
+  defmacro __using__(_) do
+    quote do
+      def action(%Plug.Conn{assigns: %{current_user: user}} = conn, _opts) do
+        apply(__MODULE__, action_name(conn), [conn, conn.params, user])
+      end
+    end
+  end
+end
